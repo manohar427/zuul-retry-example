0ede18ace7ab48398c7bc89b0a6bec5da027293d
unknown <manohar427@gmail.com>
2017-10-15 21:19:41 +0530

    zull retry

---
+++ zuul-retryer_1/src/main/java/com/example/ZuulRetryerApplication.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import com.example.exclude.RibbonConfig;
+import com.example.filter.SimpleFilter;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableZuulProxy
+@RibbonClients(defaultConfiguration = RibbonConfig.class)
+@ComponentScan(basePackages = "com.example",
+		excludeFilters =@ComponentScan.Filter(
+				type = FilterType.REGEX,
+				pattern = "com.example.exclude.*"))
+public class ZuulRetryerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulRetryerApplication.class, args);
+	}
+
+	static class ServiceException extends RuntimeException {
+		public ServiceException(String reason) {
+			super(reason);
+		}
+	}
+	
+	 @Bean
+	  public SimpleFilter simpleFilter() {
+	    return new SimpleFilter();
+	  }
+}
